080c865919e30e62eef527d8c380790e9a2b60e1
NikoP93 <nipe0001@stud.kea.dk>
2024-11-01 15:14:48 +0100

    Lavet post Rider

---
+++ js/modulejson.js
@@ -0,0 +1,19 @@
+function fetchAnyUrl(url){
+    return fetch(url).then(response => response.json());
+}
+
+async function postObjectAsJson(url,object,httpVerbum){
+    const objectAsJsonString = JSON.stringify(object);
+    console.log(objectAsJsonString);
+    const fetchOptions = {
+        method: httpVerbum,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: objectAsJsonString
+    }
+    const response = await fetch(url, fetchOptions);
+    return response;
+}
+
+export{fetchAnyUrl,postObjectAsJson};
